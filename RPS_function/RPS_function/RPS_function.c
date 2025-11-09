3143b4f4fb52b743263f53de9efd802f8e10d95e
lynn339 <lynnchien0419@gmail.com>
2025-11-09 01:33:16 -0500

    first test case refactor

--- RPS_function/RPS_function/RPS_function.c
+++ RPS_function/RPS_function/RPS_function.c
@@ -6,6 +6,10 @@ const char* determineWinner(const char* player1, const char* player2)
 {
     if (strcmp(player1, player2) == 0)
     {
-        return "Draw";
+        if (strcmp(player1, "Rock") == 0 || strcmp(player1, "Paper") == 0 || strcmp(player1, "Scissors") == 0)
+            return "Draw";
+        else
+            return "Invalid";
     }
+
 }
