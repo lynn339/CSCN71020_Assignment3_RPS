2ced4b43965498a93e7990b9d92a4865ef907419
lynn339 <lynnchien0419@gmail.com>
2025-11-09 02:47:18 -0500

    delete the original if statement for draw situation and add the new refactor one

--- RPS_function/RPS_function/RPS_function.c
+++ RPS_function/RPS_function/RPS_function.c
@@ -14,10 +14,8 @@ const char* determineWinner(const char* player1, const char* player2)
         return "Invalid";
     }
 
-    if (strcmp(player1, player2) == 0)
-    {
-        if (strcmp(player1, "Rock") == 0 || strcmp(player1, "Paper") == 0 || strcmp(player1, "Scissors") == 0)
-            return "Draw";
+    if (strcmp(player1, player2) == 0) {
+        return "Draw";
     }
     if ((strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0) ||
         (strcmp(player1, "Scissors") == 0 && strcmp(player2, "Paper") == 0) ||
