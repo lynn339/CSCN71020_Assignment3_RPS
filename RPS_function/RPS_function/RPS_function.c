f05d73631283757ad5fd4f8f43eca60072c72895
lynn339 <lynnchien0419@gmail.com>
2025-11-09 01:37:35 -0500

    test case 2(testplayer1win) pass version with function code

--- RPS_function/RPS_function/RPS_function.c
+++ RPS_function/RPS_function/RPS_function.c
@@ -11,5 +11,11 @@ const char* determineWinner(const char* player1, const char* player2)
         else
             return "Invalid";
     }
+    if ((strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0) ||
+        (strcmp(player1, "Scissors") == 0 && strcmp(player2, "Paper") == 0) ||
+        (strcmp(player1, "Paper") == 0 && strcmp(player2, "Rock") == 0))
+    {
+        return "Player1";
+    }
 
 }
