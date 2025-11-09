52e8d3e6933278fd02b1b471c47041b8e7b0d2b0
lynn339 <lynnchien0419@gmail.com>
2025-11-09 02:37:56 -0500

    test case 4(invalid input, the first letter is not capital) pass version

--- RPS_function/RPS_function/RPS_function.c
+++ RPS_function/RPS_function/RPS_function.c
@@ -8,8 +8,6 @@ const char* determineWinner(const char* player1, const char* player2)
     {
         if (strcmp(player1, "Rock") == 0 || strcmp(player1, "Paper") == 0 || strcmp(player1, "Scissors") == 0)
             return "Draw";
-        else
-            return "Invalid";
     }
     if ((strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0) ||
         (strcmp(player1, "Scissors") == 0 && strcmp(player2, "Paper") == 0) ||
@@ -23,4 +21,10 @@ const char* determineWinner(const char* player1, const char* player2)
     {
         return "Player2";
     }
+    if (strcmp(player1, "rock") == 0 || strcmp(player1, "paper") == 0 || strcmp(player1, "scissors") == 0)
+    {
+        return "Invalid";
+    }
+   
+
 }
